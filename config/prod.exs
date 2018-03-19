1f9f604cf853c8ca95697049750d8657a1b1c04a
Desmond Bowe <desmond@crevalle.io>
2018-03-19 16:04:36 -0700

    set port in config

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :elixir_talk, ElixirTalkWeb.Endpoint,
-  load_from_system_env: true,
   url: [host: "elixirtalk.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
